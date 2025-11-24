515f452e426463b9c82dcb9aacfd2ca75f17bb76
Leandro Marc <leandromarcjt@gmail.com>
2025-11-23 22:27:53 -0300

    construção da tabela cliente transferida para base de dados

--- SQL/cliente.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE CLIENTE (
-    id_cliente int primary key auto_increment,
-    nome VARCHAR(100) not null,
-    cpf char(11) not null UNIQUE,
-    dt_nasc date not null,
-    telefone char(11) not null, 
-    endereco VARCHAR(500) not null
-);
