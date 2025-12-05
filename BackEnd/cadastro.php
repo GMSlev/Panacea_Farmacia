ca9bf3d95b448c6f91538cf48c75768d262e93ab
Leandro Marc <leandromarcjt@gmail.com>
2025-12-04 21:48:32 -0300

    Correção do processamento de cadastro
    Correções foram realizadas na parte  da caputura de dados inseridos no form, para realizar o processamento do cadastro, todos os campos agoram coincidem com as colunas do banco de dados."nome ,usuario ,senha ,email e data de cadastro".
    
    Também foi retirada a opção que gerava um nome/usuario com base nas primeiras letras do e-mail cadastrado.
    Pois isso estava impedindo de usar o RA dos alunos como exemplo número de Usuario.

--- BackEnd/cadastro.php
+++ BackEnd/cadastro.php
@@ -1,62 +1,52 @@
 <?php
-require "conexao.php";
+include ('conexao.php'); // aqui vc inclui o arquivo de conexão com o banco de dados
+
+$nome = $_POST['nome'] ?? '';
+$usuario = $_POST['usuario'] ?? ''; // como exemplo, utilizamos 9 caracteres para o usuário, que é qntdd do RA dos alunos.
+$senha = $_POST['senha'] ?? '';
+$email = $_POST['email'] ?? '';
+$dataCadastro = $_POST['data'] ?? '';
+
+// as variaveis acima, recebem os valores inseridos na página "cadastro.html".
 
 
-$nome     = $_POST['nome'] ?? '';
-$email    = $_POST['email'] ?? '';
-$senha    = $_POST['senha'] ?? '';
+    if (empty($nome) || empty($email) || empty($senha) || empty($dataCadastro) || empty($usuario)) {
+        die("Preencha todos os campos obrigatórios."); // essa parte do código verifica se algum campo ficou vazio
+                                                       // se ss, ele exibe a mensagem e encerra o processamento
+    }
 
+$verificaEmail = $conecta->prepare("SELECT id_user FROM USUARIOS WHERE email = ?"); // prepara o comando SQL de consultar no banco de dadaos a coluna email
+$verificaEmail->bind_param("s", $email); // vincula o parâmetro de email ao comando SQL, indicando que é uma string ("s").
+$verificaEmail->execute(); // roda o comando sql que no caso é a consulta (SELECT)!!
+$verificaEmail->store_result(); // ao finalizar a consulta, ele armazena o valor para a variável $verificaEmail.
 
-if (empty($nome) || empty($email) || empty($senha)) {
-    die("Preencha todos os campos obrigatórios.");
+// a estrutura acima verifica se o email já está cadastrado no banco de dados
+
+if ($verificaEmail->num_rows > 0) { // aqui ele verifica as linhas na tabela do banco de dados que ja possuem o email informado
+    die("Este usuário já está cadastrado no sistema.<br><p>Voltar para <a href='../FrontEnd/cadastro.html'>Cadastro</a></p>");
 }
 
-
-$sqlCheck = $conn->prepare("SELECT id_user FROM USUARIOS WHERE email = ?");
-$sqlCheck->bind_param("s", $email);
-$sqlCheck->execute();
-$sqlCheck->store_result();
-
-if ($sqlCheck->num_rows > 0) {
-    die("Este e-mail já está cadastrado no sistema.");
-}
-
-
-$base = explode("@", $email)[0]; 
-$base = strtoupper(substr($base, 0, 5)); 
-
-
-$sqlCount = $conn->query("SELECT COUNT(*) AS total FROM USUARIOS");
-$total = $sqlCount->fetch_assoc()['total'] + 1;
-
-$numero = str_pad($total, 4, "0", STR_PAD_LEFT); 
-$usuario = $base . $numero;
-
-
-$usuario = substr($usuario, 0, 9);
-
-
-$senhaHash = password_hash($senha, PASSWORD_DEFAULT);
-
+$senhaHash = password_hash($senha, PASSWORD_DEFAULT); // transforma a senha em método criptografado
 
 $dataCadastro = date("Y-m-d");
 
-
-$sqlInsert = $conn->prepare("
+$inserirDados = $conecta->prepare("
     INSERT INTO USUARIOS (nome, usuario, senha_hash, email, data_cadastro)
-    VALUES (?, ?, ?, ?, ?)
-");
+    VALUES (?, ?, ?, ?, ?) 
+"); // da mesma maneira que a consulta, aqui ele prepara o comando SQL para inserir os dados no banco de dados
 
-$sqlInsert->bind_param("sssss", $nome, $usuario, $senhaHash, $email, $dataCadastro);
+$inserirDados->bind_param("sssss", $nome, $usuario, $senhaCriptografada, $email, $dataCadastro);
 
-if ($sqlInsert->execute()) {
+if ($inserirDados->execute()) { // executa o comando SQL de inserção (INSERT)
     echo "<h3>Funcionário cadastrado com sucesso!</h3>";
     echo "Nome: $nome <br>";
     echo "Email: $email <br>";
     echo "Usuário gerado: <b>$usuario</b><br>";
+    echo '<p>Iniciar Sessão <a href="../index.html">Fazer login</a></p>'; // apos cadastro mostra os dados e link para login
 } else {
-    echo "Erro ao cadastrar: " . $conn->error;
+    echo "Erro ao cadastrar: " . $conecta->error; // caso aconteca algum erro na inserção ou conexão, ele exibe a mensagem de erro
+    
 }
 
-$conn->close();
+$conecta->close();
 ?>
